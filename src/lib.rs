06fd5606f0e380f1d61eee94a11becedf1a910b1
Sean Griffin <sean@seantheprogrammer.com>
2016-02-04 14:01:54 -0700

    Decouple the library from PG
    I avoided this before because I thought it was saying that `type
    Connection` needed to be sync. That was not the problem, it was that the
    manage needs to be `Sync`, and `PhantomData<T>` is only `Sync` if `T` is
    `Sync`.

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,23 @@ extern crate diesel;
 extern crate r2d2;
 
 use diesel::{Connection, ConnectionError};
-use diesel::pg::PgConnection;
 use r2d2::ManageConnection;
 use std::convert::Into;
+use std::marker::PhantomData;
 
-pub struct ConnectionManager {
+pub struct ConnectionManager<T> {
     database_url: String,
+    _marker: PhantomData<T>,
 }
 
-impl ConnectionManager {
+unsafe impl<T: Send + 'static> Sync for ConnectionManager<T> {
+}
+
+impl<T> ConnectionManager<T> {
     pub fn new<S: Into<String>>(database_url: S) -> Self {
         ConnectionManager {
             database_url: database_url.into(),
+            _marker: PhantomData,
         }
     }
 }
@@ -23,20 +28,22 @@ pub enum Error {
     QueryError(diesel::result::Error),
 }
 
-impl ManageConnection for ConnectionManager {
-    type Connection = PgConnection;
+impl<T> ManageConnection for ConnectionManager<T> where
+    T: Connection + Send + 'static,
+{
+    type Connection = T;
     type Error = Error;
 
-    fn connect(&self) -> Result<PgConnection, Error> {
-        PgConnection::establish(&self.database_url)
+    fn connect(&self) -> Result<T, Error> {
+        T::establish(&self.database_url)
             .map_err(Error::ConnectionError)
     }
 
-    fn is_valid(&self, conn: &mut PgConnection) -> Result<(), Error> {
+    fn is_valid(&self, conn: &mut T) -> Result<(), Error> {
         conn.execute("SELECT 1").map(|_| ()).map_err(Error::QueryError)
     }
 
-    fn has_broken(&self, _conn: &mut PgConnection) -> bool {
+    fn has_broken(&self, _conn: &mut T) -> bool {
         false
     }
 }
