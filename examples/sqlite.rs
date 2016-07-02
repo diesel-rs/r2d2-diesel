737e67f9c2803adcb492193a4895b005783a7ab8
Markus Kohlhase <mail@markus-kohlhase.de>
2016-07-03 00:58:05 +0200

    add SQLite support

---
+++ examples/sqlite.rs
@@ -0,0 +1,23 @@
+extern crate diesel;
+extern crate r2d2;
+extern crate r2d2_diesel;
+
+use std::thread;
+
+use diesel::sqlite::SqliteConnection;
+use r2d2_diesel::ConnectionManager;
+
+fn main() {
+    let config = r2d2::Config::default();
+    let manager = ConnectionManager::<SqliteConnection>::new("db.sqlite");
+    let pool = r2d2::Pool::new(config, manager).expect("Failed to create pool.");
+
+    for _ in 0..10i32 {
+        let pool = pool.clone();
+        thread::spawn(move || {
+            let connection = pool.get();
+
+            assert!(connection.is_ok());
+        });
+    }
+}
