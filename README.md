ff6f90fd21e4ef848690f075570d2986c93358f5
wangsijie <wangsijie@bytedance.com>
2018-01-14 21:31:35 +0800

    Add new_with_init_operations function for ConnectionManager

--- README.md
+++ README.md
@@ -40,6 +40,48 @@ fn main() {
 }
 ```
 
+Or you may need some extra operations after establishing a connection
+```rust
+extern crate diesel;
+extern crate r2d2;
+extern crate r2d2_diesel;
+
+use std::thread;
+use diesel::Connection;
+use diesel::RunQueryDsl;
+use diesel::types::Text;
+use diesel::expression::sql_literal::sql;
+use diesel::sqlite::SqliteConnection;
+use r2d2_diesel::{ConnectionManager, Error};
+
+const CACHE_SIZE: &'static str = "4000";
+fn init_operations(connection: &SqliteConnection) -> Result<(), Error> {
+    let query = format!("PRAGMA CACHE_SIZE = {}", CACHE_SIZE);
+    connection.execute(&query).map_err(Error::QueryError)?;
+
+    Ok(())
+}
+
+fn main() {
+    let manager =
+        ConnectionManager::<SqliteConnection>::new_with_init_operations("db.sqlite",
+                                                                        Box::new(init_operations));
+    let pool = r2d2::Pool::builder().build(manager).expect("Failed to create pool.");
+
+    for _ in 0..10i32 {
+        let pool = pool.clone();
+        thread::spawn(move || {
+            let query = "PRAGMA CACHE_SIZE";
+            let connection = pool.get().expect("Failed to get pooled connection");
+            let cache_size = sql::<Text>(&query).load::<String>(&*connection)
+                .expect("Failed to get cache size").pop();
+
+            assert_eq!(Some(CACHE_SIZE.to_owned()), cache_size);
+        });
+    }
+}
+```
+
 Using diesel master branch
 ============================
 
--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,35 @@ use std::marker::PhantomData;
 
 pub struct ConnectionManager<T> {
     database_url: String,
+    init_operations: Option<BoxedInitOperations<T>>,
     _marker: PhantomData<T>,
 }
 
 unsafe impl<T: Send + 'static> Sync for ConnectionManager<T> {
 }
 
+pub trait InitOperations<T>: Fn(&T) -> Result<(), Error> + Sync + 'static + Send {}
+impl<T, F> InitOperations<T> for F where F: Fn(&T) -> Result<(), Error> + Sync + 'static + Send {}
+pub type BoxedInitOperations<T> = Box<InitOperations<T, Output=Result<(), Error>>>;
+
 impl<T> ConnectionManager<T> {
-    pub fn new<S: Into<String>>(database_url: S) -> Self {
+    pub fn new<S: Into<String>>(database_url: S) -> Self
+    {
         ConnectionManager {
             database_url: database_url.into(),
+            init_operations: None,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn new_with_init_operations<S: Into<String>>(
+        database_url: S,
+        init_operations: BoxedInitOperations<T>
+    ) -> Self
+    {
+        ConnectionManager {
+            database_url: database_url.into(),
+            init_operations: Some(init_operations),
             _marker: PhantomData,
         }
     }
@@ -55,8 +74,12 @@ impl<T> ManageConnection for ConnectionManager<T> where
     type Error = Error;
 
     fn connect(&self) -> Result<T, Error> {
-        T::establish(&self.database_url)
-            .map_err(Error::ConnectionError)
+        let conn = T::establish(&self.database_url).map_err(Error::ConnectionError)?;
+        if let Some(init_operations) = self.init_operations.as_ref() {
+            init_operations(&conn)?;
+        }
+
+        Ok(conn)
     }
 
     fn is_valid(&self, conn: &mut T) -> Result<(), Error> {
